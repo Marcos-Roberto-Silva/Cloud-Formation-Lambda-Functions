7eb2262dadc6847271818186623c9418e4a2aaf2
Marcos Silva <marcossilva@Marcoss-Notebook.local>
2024-06-24 17:34:37 -0300

    refactor

--- lib/e-commerceApi-stack.ts
+++ lib/e-commerceApi-stack.ts
@@ -1,15 +1,41 @@
 import * as lambdaNodeJs from "aws-cdk-lib/aws-lambda-nodejs";
-import * as apiGateway from "aws-cdk-lib/aws-apigateway";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 import * as cwlogs from "aws-cdk-lib/aws-logs";
 import * as cdk from "aws-cdk-lib";
 import {Construct} from "constructs";
 
+interface ECommerceApiStackprops extends cdk.StackProps {
+    productsFetchHandler: lambdaNodeJs.NodejsFunction
+}
+
 export class ECommerceApiStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: Construct, id: string, props: ECommerceApiStackprops) {
         super(scope, id, props)
 
-        const api = new apiGateway.RestApi(this, "ECommerceApi", {
-            restApiName: "ECommerceApi"
+        const logGroup = new cwlogs.LogGroup(this, "EcommerceApiLogs")
+        
+        const api = new apigateway.RestApi(this, "EcommerceApi", {
+            restApiName: "EcommerceApi", 
+            cloudWatchRole: true,
+            deployOptions: {
+                accessLogDestination: new apigateway.LogGroupLogDestination(logGroup),
+                accessLogFormat: apigateway.AccessLogFormat.jsonWithStandardFields({
+                    httpMethod: true,
+                    ip: true,
+                    protocol: true,
+                    requestTime: true,
+                    resourcePath: true,
+                    responseLength: true,
+                    status: true,
+                    caller: true,
+                    user: true
+                })
+            },  
         })
+
+        const productsFetchIntegration =  new apigateway.LambdaIntegration(props.productsFetchHandler);
+
+        const productsResource = api.root.addResource("products");
+        productsResource.addMethod("GET", productsFetchIntegration);''
     }
 }
