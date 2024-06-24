e908b788d2d13028c364fe37131b33b9a8eb2da9
Marcos Silva <marcossilva@Marcoss-Notebook.local>
2024-06-24 14:40:27 -0300

    create apiGateway integration

--- lib/e-commerceApi-stack.ts
+++ lib/e-commerceApi-stack.ts
@@ -4,12 +4,21 @@ import * as cwlogs from "aws-cdk-lib/aws-logs";
 import * as cdk from "aws-cdk-lib";
 import {Construct} from "constructs";
 
+interface ECommerceApiStackprops extends cdk.StackProps {
+    productsFetchHandler: lambdaNodeJs.NodejsFunction
+}
+
 export class ECommerceApiStack extends cdk.Stack {
-    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    constructor(scope: Construct, id: string, props: ECommerceApiStackprops) {
         super(scope, id, props)
 
         const api = new apiGateway.RestApi(this, "ECommerceApi", {
             restApiName: "ECommerceApi"
         })
+
+        const productsFetchIntegration =  new apiGateway.LambdaIntegration(props.productsFetchHandler);
+
+        const productsResource = api.root.addResource("products");
+        productsResource.addMethod("GET", productsFetchIntegration)
     }
 }
